88d0d2dc572d8a7e925bf6aa8d714f45b4fc3ef8
usuario <usuario@10.22.143.26>
2014-12-04 13:54:43 +0000

    Versión 1

--- src/Practica3/MultiplicacionRusa.java
+++ src/Practica3/MultiplicacionRusa.java
@@ -11,7 +11,20 @@ public class MultiplicacionRusa implements Multiplicacion {
     }
 
     @Override
-    public BigInteger multiplica(BigInteger n, BigInteger v) {
-        return null;
+    public BigInteger multiplica(BigInteger n, BigInteger m) {
+        BigInteger tupla [] = new BigInteger[2];
+        if(n.compareTo(new BigInteger("1"))>=0){
+            if (n.mod(new BigInteger("2"))!=BigInteger.ZERO){
+                tupla[0] = n.divide(new BigInteger("2"));
+                tupla[1] = m.multiply(new BigInteger("2"));
+                return m.add(multiplica(tupla[0], tupla[1]));
+            }
+            else{
+                tupla[0]=n.divide(new BigInteger("2"));
+                tupla[1]=m.multiply(new BigInteger("2"));
+                return multiplica(tupla[0], tupla[1]);
+            }
+        }
+        return BigInteger.ZERO;
     }
 }
