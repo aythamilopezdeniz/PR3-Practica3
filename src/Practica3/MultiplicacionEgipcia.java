2e83eadbf76d322a0bfe02daa6f26207e7067a7e
Aythami <Aythami@Aythami-VAIO>
2014-12-16 19:44:35 +0000

    Versión 1

--- src/Practica3/MultiplicacionEgipcia.java
+++ src/Practica3/MultiplicacionEgipcia.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 public class MultiplicacionEgipcia implements Multiplicacion {
     private final String nombreMetodo="Multiplicación Egipcia";
+    private BigInteger[] tupla;
 
     @Override
     public String nombreMetodo() {
@@ -12,15 +13,14 @@ public class MultiplicacionEgipcia implements Multiplicacion {
 
     @Override
     public BigInteger multiplica(BigInteger n, BigInteger m) {
-        BigInteger[] tupla=MultiplicacionEgipcia(n, m, new BigInteger("1"));
-        return tupla[0];
+        BigInteger[] tuplaEgipcia=MultiplicacionEgipcia(n, m, new BigInteger("1"));
+        return tuplaEgipcia[0];
     }
     private BigInteger[] MultiplicacionEgipcia(BigInteger n, BigInteger m, BigInteger doble1) {
-        BigInteger[] tupla=null;
         if(doble1.compareTo(m)==1)
             return new BigInteger[]{new BigInteger("0"),new BigInteger("0")};
-        if(doble1.compareTo(m)<=0){
-            tupla[0]=MultiplicacionEgipcia(n, m, doble1.multiply(new BigInteger("2")))[0];
+        else {
+            tupla=MultiplicacionEgipcia(n, m, doble1.multiply(new BigInteger("2")));
             if(tupla[1].add(doble1).compareTo(m)<=0){
                 tupla[0]=tupla[0].add(n.multiply(doble1));
                 tupla[1]=tupla[1].add(doble1);
@@ -28,6 +28,5 @@ public class MultiplicacionEgipcia implements Multiplicacion {
             }else
                 return tupla;
         }
-        return tupla;
     }
 }
--- src/Practica3/Practica3.java
+++ src/Practica3/Practica3.java
@@ -1,7 +1,64 @@
 package Practica3;
 
+import java.math.BigInteger;
+
 public class Practica3 {
 
     public static void main(String[] args) {
+        int[] v={6,10,555,8000,16000,320000};
+        final int constante = 10;
+        String imprimir = "------";
+        float[] tiempo_suma=new float[3];
+        float[] tiempo_rusa=new float [6];
+        float[] tiempo_egipcia=new float[6];
+        BigInteger[] resultado_suma=new BigInteger[3];
+        BigInteger[] resultado_rusa=new BigInteger[3];
+        BigInteger[] resultado_egipcia=new BigInteger[3];
+        for(int i=0;i<3;i++){
+            double antes=System.currentTimeMillis();
+            resultado_suma[i]=new MultiplicacionSumasSucesivas().multiplica(
+                    BigInteger.valueOf(v[i]), BigInteger.valueOf((constante)));
+            double despues=System.currentTimeMillis();
+            tiempo_suma[i]=(float)((despues-antes)/1000);
+            antes=System.currentTimeMillis();
+            resultado_rusa[i]=new MultiplicacionRusa().multiplica(
+                    BigInteger.valueOf(v[i]),BigInteger.valueOf((constante)));
+            despues=System.currentTimeMillis();
+            tiempo_rusa[i]=(float)((despues-antes)/1000);
+            antes=System.currentTimeMillis();
+            resultado_egipcia[i]=new MultiplicacionEgipcia().multiplica(
+                    BigInteger.valueOf(v[i]),BigInteger.valueOf((constante)));
+            despues=System.currentTimeMillis();
+            tiempo_egipcia[i]=(float)((despues-antes)/1000);
+        }
+        for(int i=3;i<6;i++){
+            double antes=System.currentTimeMillis();
+            new MultiplicacionRusa().multiplica(
+                    BigInteger.valueOf(v[i]),BigInteger.valueOf((constante)));
+            double despues=System.currentTimeMillis();
+            tiempo_rusa[i]=(float)((despues-antes)/1000);
+            antes=System.currentTimeMillis();
+            new MultiplicacionEgipcia().multiplica(
+                    BigInteger.valueOf(v[i]),BigInteger.valueOf((constante)));
+            despues=System.currentTimeMillis();
+            tiempo_egipcia[i]=(float)((despues-antes)/1000);
+         }
+         System.out.println("Tamaño de las ristras    Tiempo Sumas    Tiempo rusa    Tiempo Egipcia");
+         for(int i=0;i<v.length;i++) {
+            if(i>2){
+              System.out.printf("%21d    %13s    %12f    %14f\n", 
+                    v[i],imprimir,tiempo_rusa[i],tiempo_egipcia[i]);
+                
+            }else{
+            System.out.printf("%21d    %13f    %12f    %14f\n", 
+                    v[i], tiempo_suma[i],tiempo_rusa[i],tiempo_egipcia[i]);
+            }
+     
+          }
+          System.out.println("Tamaño de las ristras      Resultados Sumas      Resultados rusa      Resultados Egipcia");
+          for(int i=0;i<3;i++) {
+              System.out.printf("%21d    %13s    %12s    %14s\n", v[i],resultado_suma[i].toString(),
+                      resultado_rusa[i].toString(),resultado_egipcia[i].toString());
+          }
     }
 }
